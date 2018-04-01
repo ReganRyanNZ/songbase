8fdf6b438b7626fd8577f3887d094c1ba57ddad2
Regan Ryan <regan.ryan.nz@gmail.com>
2018-04-01 12:01:59 +1200

    fixed hyphens and dashes in eng hymnal source

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180323030004) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "audits", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "song_id"
