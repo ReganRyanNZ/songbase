d8856b53f96be9c80f67d4758ad8352d3debebf2
Regan Ryan <regan.ryan.nz@gmail.com>
2020-06-27 00:59:26 +1200

    changed slash to space for print regex

--- app/assets/javascripts/components/SongPrint.jsx
+++ app/assets/javascripts/components/SongPrint.jsx
@@ -7,7 +7,7 @@ class SongPrint extends React.Component {
     var lyrics = this.props.lyrics;
     var removeChordsSplitter = /\[.*?\]/; // using .split() will remove everything within square brackets
     var getChordsFilter = /(^|\])[^[]*\[?/g; // using replace() will remove everything not in square brackets
-    lyrics = lyrics.replace(/(^|\n)\s\s/g, "$1    "); // pad out choruses to 4 spaces
+    lyrics = lyrics.replace(/(^|\n)  /g, "$1    "); // pad out choruses to 4 spaces
       var lines = lyrics.split("\n");
     for(i=0; i < lines.length; i++) {
       var line = lines[i].split(removeChordsSplitter);
