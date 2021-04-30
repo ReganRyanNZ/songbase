431eec65affcc57e7352885a4415281ca12e817e
Regan Ryan <Regan.Ryan008@msd.govt.nz>
2021-04-30 15:33:46 +1200

    Fixed asset compilation for staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,13 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  # config.assets.compress = false
+  # config.assets.debug = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
