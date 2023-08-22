b7a2e2cc1165f4ffd07518ba5147d6eb2381c1ac
Regan Ryan <regan.ryan.nz@gmail.com>
2023-08-22 18:16:32 -0300

    added a warning re client cache in system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,11 +1,17 @@
 require "test_helper"
 
+# WARNING:
+#
+# System tests will include using the client's IndexedDB cache, which doesn't
+# reset between tests like FactoryBot records. Best to start tests with
+# visiting settings and clicking on the "reset cache" button.
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # If you want to see the browser window open up and go through all the
   # motions, use this line instead:
   #
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   Capybara.default_max_wait_time = 2
 
