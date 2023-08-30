c698be164c60c6aadf6bc4102915af06edb1e6bc
Regan Ryan <regan.ryan.nz@gmail.com>
2023-08-30 18:37:59 -0300

    Rails and gems upgrade

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,9 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/rails"
+require 'minitest/reporters'
 require 'factory_bot_rails'
 
 class ActiveSupport::TestCase
-  require 'minitest/reporters'
-  MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
+  Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 end
