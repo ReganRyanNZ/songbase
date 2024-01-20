3f973670593c53fcd7bf9bfdd860e904244ad6de
Regan Ryan <regan.ryan.nz@gmail.com>
2024-01-20 17:12:13 -0300

    fixed body being shorter than 100vh. Removed outdated config

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Songbase
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.0
-    config.active_record.legacy_connection_handling = false
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
