3f7e1e06505c5c577b10317b3d9842ec44e60de3
Xuthus2022 <readjethro@gmail.com>
2025-06-03 18:27:01 +1200

    updating the backend for book admin, list creating new books.. overrode book form

--- app/assets/javascripts/components/AdminBookDisplay.jsx
+++ app/assets/javascripts/components/AdminBookDisplay.jsx
@@ -1,4 +1,4 @@
-class AdminSongDisplay extends React.Component {
+class AdminBookDisplay extends React.Component {
   constructor(props) {
     super(props);
 
@@ -8,45 +8,43 @@ class AdminSongDisplay extends React.Component {
     };
   }
   render() {
-  const book = this.state.book;
+    const book = this.state.book;
+    // Sort songs array by their reference in the book (songRef)
+    const sortedSongs = this.state.songs.slice().sort((a, b) => {
+      const refA = book && book.songs && book.songs[a.id] ? book.songs[a.id] : Infinity;
+      const refB = book && book.songs && book.songs[b.id] ? book.songs[b.id] : Infinity;
+      return refA - refB;
+    });
 
-  // Sort songs array by their reference in the book (songRef)
-  const sortedSongs = this.state.songs.slice().sort((a, b) => {
-    const refA = book && book.songs && book.songs[a.id] ? book.songs[a.id] : Infinity;
-    const refB = book && book.songs && book.songs[b.id] ? book.songs[b.id] : Infinity;
-    return refA - refB;
-  });
+    let list = sortedSongs.map((song) => {
+      let editRef = "/songs/" + song.id + "/edit";
+      let songRef = book && book.songs && book.songs[song.id] ? book.songs[song.id] : "-";
 
-  let list = sortedSongs.map((song) => {
-    let editRef = "/songs/" + song.id + "/edit";
-    let songRef = book && book.songs && book.songs[song.id] ? book.songs[song.id] : "-";
+      return (
+        <tr key={song.id}>
+          <td>
+            #{songRef}
+            &nbsp;&nbsp;
+          </td>
+          <td>
+            <a href={editRef}>{song.title}</a>
+          </td>
+          <td>
+            <div className="last_edited">{song.last_editor}</div>
+          </td>
+          <td>
+            <div className="edit_timestamp">{song.edit_timestamp}</div>
+          </td>
+        </tr>
+      );
+    });
 
     return (
-      <tr key={song.id}>
-       <td>
-          #{songRef}
-          &nbsp;&nbsp;
-        </td>
-        <td>
-          <a href={editRef}>{song.title}</a>
-        </td>
-        <td>
-          <div className="last_edited">{song.last_editor}</div>
-        </td>
-        <td>
-          <div className="edit_timestamp">{song.edit_timestamp}</div>
-        </td>
-      </tr>
+      <div className="admin_list">
+        <table className="admin_table">
+          <tbody>{list}</tbody>
+        </table>
+      </div>
     );
-  });
-
-  return (
-    <div className="admin_list">
-      <table className="admin_table">
-        <tbody>{list}</tbody>
-      </table>
-    </div>
-  );
-}
-
+  }
 }
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,33 @@ class BooksController < ApplicationController
     @book = Book.find(params[:book_slug])
     song_ids = @book.songs.keys.map(&:to_i)
     @songs = Song.where(id: song_ids)
-end
+  end
 
   def admin
     @books = Book.all()
+    @users = User.all()
   end
 
   def new
+    @book = Book.new
+    @songs = Song.all()
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @songs = Song.all()
   end
 
   def create
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book, notice: "Book was successfully updated"
+    else
+      render :edit
+    end
   end
 end
