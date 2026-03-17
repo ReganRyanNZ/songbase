f4d19bb88d7a042c55b3a2c91383ba1cd22fb8c9
Jethro <readjethro@gmail.com>
2026-03-17 19:37:16 +1300

    Add fill attribute to KabobIcon SVG for consistent styling

--- app/assets/javascripts/components/icons/KabobIcon.jsx
+++ app/assets/javascripts/components/icons/KabobIcon.jsx
@@ -1,5 +1,5 @@
 const KabobIcon = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
     <circle cx="12" cy="5" r="1.5"/>
     <circle cx="12" cy="12" r="1.5"/>
     <circle cx="12" cy="19" r="1.5"/>
