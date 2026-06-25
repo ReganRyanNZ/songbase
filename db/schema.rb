87c8b14d95661182072a6f8119cce5afb561729d
Jethro <readjethro@gmail.com>
2026-06-25 02:45:05 +0000

    chore: update gem dependencies and remove pg_stat_statements extension

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_07_090324) do
   create_schema "songbase_production"
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
